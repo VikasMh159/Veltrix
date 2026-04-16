f8183b6fe9535d704c2c024681553e423b775353
Vikas Mahato <mvvikas159@gmail.com>
2026-04-16 12:12:59 +0530

    Switch app to native Kotlin flow

--- app/src/main/java/com/veltrix/MainActivity.kt
+++ app/src/main/java/com/veltrix/MainActivity.kt
@@ -4,20 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.veltrix.ui.theme.VeltrixTheme
+import com.veltrix.viewmodel.MainViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            VeltrixTheme(darkTheme = true) {
-                WebAppScreen(
-                    activity = this,
-                    modifier = Modifier.fillMaxSize()
-                )
+            val mainViewModel: MainViewModel = viewModel()
+            val state by mainViewModel.uiState.collectAsStateWithLifecycle()
+
+            VeltrixTheme(darkTheme = state.isDarkMode) {
+                VeltrixApp(viewModel = mainViewModel)
             }
         }
     }
